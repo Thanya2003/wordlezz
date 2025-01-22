670a7db297b7c11d56f49562b754c43dbbb48069
thanya2003 <thanyashettigar@gmail.com>
2025-01-22 17:39:42 +0530

    Grid

---
+++ src/component/grid.jsx
@@ -0,0 +1,20 @@
+import '../styles/grid.scss'
+function Grid(props){
+    const tiles= [...Array(props.height).keys()].map((i, _)=>
+        <div className="row" key={`${i}`}>
+        {[...Array(props.width).keys()].map((j)=>{
+            return(<div className='tile' key={`${i}-${j}`}>{`${i}, ${j}`}</div>)
+        })}
+    </div>
+        );
+    return(
+        <>
+        <div className='grid-container'>
+            <div className="tiles-container">
+           {tiles}
+           </div>
+        </div>
+        </>
+    )
+}
+export default Grid
--- src/wordle.jsx
+++ src/wordle.jsx
@@ -1,9 +1,13 @@
-import Header from './component/header'
+import Header from './component/header.jsx'
 import './styles/wordle.css'
+import Grid from './component/grid.jsx'
 function wordle(){
+    const width=5
+    const height=6
     return(
         <div className="app-cont">
            <Header/> 
+           <Grid width={width} height={height}/>
         </div>
         
     )
